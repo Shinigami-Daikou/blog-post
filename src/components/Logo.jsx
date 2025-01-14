31e95c2c33d4f644878e6a11db8faa6fe98bc28e
Shinigami-Daikou <chauhanbhupendra145@gmail.com>
2025-01-14 16:40:22 +0530

    Typo fixed

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import LogoImage from '../assets/logo.bmp'
 
 function Logo({width = '100px'}) {
   return (
     <div>
-      <img src='src\assets\logo.bmp' alt='Logo' width={width}/>
+      <img src={LogoImage} alt='Logo' width={width}/>
     </div>
   )
 }
